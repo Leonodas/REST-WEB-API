f45463973f6159464aa4412500687063284ae8d0
Leonodas <94011856+Leonodas@users.noreply.github.com>
2021-11-09 20:29:20 +0100

    Add files via upload

---
+++ REST Web API ELO/Controllers/auftraegeController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using REST_Web_API_ELO.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace REST_Web_API_ELO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class auftraegeController : ControllerBase
+    {
+        public static string ausgabe;
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            yield return "Auftragsnummer in URL eingeben!";
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public IEnumerable<string> Get(string id)
+        {
+
+            var Auftragsnummer = id;
+
+            string URI_KEYWORDING = "http://elo.helmut-meeth.com:9090/ELOrest-12.04.000/api/search?where=KEYWORDING&words=" + Auftragsnummer;
+
+            var request = (HttpWebRequest)WebRequest.Create(URI_KEYWORDING);
+
+            request.Method = "GET";
+            // request.UserAgent = RequestConstants.UserAgentValue;
+            //  request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+
+            var content_KEYWORDING = string.Empty;
+
+            request.Headers.Add("Authorization", "Basic ****************************");
+
+            request.ContentType = "application/json";
+            var response_KEYWORDING = (HttpWebResponse)request.GetResponse();
+            //      response.Headers.Add("X-Amz-date",DateTime.UtcNow.ToString("yyyyMMddThhmmss")+"Z");
+            using (var stream_KEYWORDING = response_KEYWORDING.GetResponseStream())
+            {
+                using (var sr_KEYWORDING = new StreamReader(stream_KEYWORDING))
+                {
+                    content_KEYWORDING = sr_KEYWORDING.ReadToEnd();
+                }
+            }
+
+            JArray jArray = new JArray();
+            jArray = JArray.Parse(content_KEYWORDING);
+
+            var result = jArray.ToObject<List<Search_Response>>();
+
+            string URI_url;
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                URI_url = "http://elo.helmut-meeth.com:9090/ELOrest-12.04.000/api/files/" + result[i].id; 
+
+                request = (HttpWebRequest)WebRequest.Create(URI_url);
+
+                request.Method = "GET";
+
+                var content_url = string.Empty;
+
+                request.Headers.Add("Authorization", "Basic ***************");
+
+                request.ContentType = "application/json";
+                var response_url = (HttpWebResponse)request.GetResponse();
+
+                using (var stream_url = response_url.GetResponseStream())
+                {
+                    using (var sr_url = new StreamReader(stream_url))
+                    {
+                        content_url = sr_url.ReadToEnd();
+                    }
+                }
+
+                File_Response result2 = JsonConvert.DeserializeObject<File_Response>(content_url);
+                String url_lokal = result2.versions[0].url;
+                var url_public = url_lokal.Replace("vsrvelo2", "elo.helmut-meeth.com");
+                ausgabe = ("Auftragsnummer: " + Auftragsnummer + "   |   " + "ID: " + result[i].id + "  |  " + "Dateiname: " + result[i].name + "  |  " + "URL: " + url_public);
+                //Console.WriteLine("\n" + "ID: " + result[i].id + "\n" + "Dateiname: " + result[i].name + "\n" + url_public + "\n");
+                //Process.Start(new ProcessStartInfo { FileName = url_public, UseShellExecute = true });
+            }
+            yield return ausgabe;    
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ REST Web API ELO/Models/Klassen.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST_Web_API_ELO.Models
+{
+    public class Search_Response
+    {
+        public string guid { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public int type { get; set; }
+        public bool isDir { get; set; }
+        public string desc { get; set; }
+        public string @lock { get; set; }
+        public string ownerName { get; set; }
+        public string access { get; set; }
+        public int parentId { get; set; }
+        public DateTime dateArchived { get; set; }
+        public DateTime? dateCustom { get; set; }
+        public DateTime dateModified { get; set; }
+    }
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class Info
+    {
+        public string guid { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public int type { get; set; }
+        public bool isDir { get; set; }
+        public string desc { get; set; }
+        public string @lock { get; set; }
+        public string ownerName { get; set; }
+        public string access { get; set; }
+        public int parentId { get; set; }
+        public DateTime dateArchived { get; set; }
+        public object dateCustom { get; set; }
+        public DateTime dateModified { get; set; }
+    }
+
+    public class Acl
+    {
+        public string member { get; set; }
+        public int id { get; set; }
+        public string type { get; set; }
+        public string access { get; set; }
+    }
+
+    public class Fields
+    {
+        public string BESTNR { get; set; }
+        public string BESTDATUM { get; set; }
+        public string BESTELLER { get; set; }
+        public string LIEFERANT { get; set; }
+        public string BRUTTO { get; set; }
+        public string NETTO { get; set; }
+        public string MWSTSATZ { get; set; }
+        public string ELO_FNAME { get; set; }
+    }
+
+    public class Map
+    {
+    }
+
+    public class Keywording
+    {
+        public int maskId { get; set; }
+        public string maskNameOriginal { get; set; }
+        public Fields fields { get; set; }
+        public Map map { get; set; }
+    }
+
+    public class Version
+    {
+        public string guid { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public string comment { get; set; }
+        public string committer { get; set; }
+        public string createdAt { get; set; }
+        public bool isWorkVersion { get; set; }
+        public string ext { get; set; }
+        public string contentType { get; set; }
+        public int size { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Content
+    {
+        public string mime { get; set; }
+        public string filename { get; set; }
+        public DateTime lastModified { get; set; }
+        public int size { get; set; }
+        public string url { get; set; }
+    }
+
+    public class File_Response
+    {
+        public Info info { get; set; }
+        public List<Acl> acl { get; set; }
+        public List<object> children { get; set; }
+        public Keywording keywording { get; set; }
+        public List<Version> versions { get; set; }
+        public Content content { get; set; }
+    }
+}
